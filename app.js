3b0aaa8b3fd03980ebaf067b7c4fbdb85fb0468a
Hai Pham <hai.dt.pham.viseohk@axa-groupsolutions.com>
2018-01-26 14:36:46 +0800

    Add header footer and error page

--- app.js
+++ app.js
@@ -27,22 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use((req, res) => res.render('error', { status: 404 }));
 
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 // config mongoose
 mongoose.Promise = Promise;
--- views/layout.hbs.tpl
+++ views/layout.hbs.tpl
@@ -8,7 +8,7 @@
     <title>{{title}}</title>
     <!-- default styles -->
     <link rel="icon" href="images/favicon.svg?v=1000" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- inject:css -->
     <!-- endinject -->
   </head>
